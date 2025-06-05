01c9fa29afbd0659323f3acb2165d8cf5d9f922d
unknown <nazarchess14@gmail.com>
2025-06-05 20:00:05 +0530

    update

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+COHERE_API_KEY = "FUOsMfgHbUK0jKJJ8K9idkp3Z2lLEKBHiIHy2IFk"
+GEMINI_API_KEY = "AIzaSyBR-N4IroPiAH0-0H9b_TzSXh3dAVYN3xA"
+GEMINI_MODEL = "gemini-2.5-flash-preview-04-17"
+# GEMINI_MODEL = "gemini-2.5-flash-preview-05-06"
