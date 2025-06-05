00f4248293718ef58a3bb33544fe130f99335974
unknown <nazarchess14@gmail.com>
2025-06-06 01:16:49 +0530

    deleted unnecessary

--- config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# config.py
-COHERE_API_KEY = "FUOsMfgHbUK0jKJJ8K9idkp3Z2lLEKBHiIHy2IFk"
-GEMINI_API_KEY = "AIzaSyBR-N4IroPiAH0-0H9b_TzSXh3dAVYN3xA"
-GEMINI_MODEL = "gemini-2.5-flash-preview-04-17"
-# GEMINI_MODEL = "gemini-2.5-flash-preview-05-06"
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
